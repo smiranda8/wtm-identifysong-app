73343f35b835b016f270d54e6f40e9db7b6927d1
smiranda8 <sara.miranda8@gmail.com>
2019-03-27 08:44:45 +0100

    server ruth actualizado

--- server-prueba3.js
+++ server-prueba3.js
@@ -22,15 +22,38 @@ var options = {
   access_secret: 'pp1LvalngHOHyLnKDzvvikuiOmIgE0brMyr2bgG5'
 };
 
+ var date = new Date(); 
+ var timestamp = date.getTime()/10000;
+ var stringToSign = buildStringToSign('POST',
+    options.endpoint,
+    options.access_key,
+    options.data_type,
+    options.signature_version,
+    timestamp);
+	
+function buildStringToSign(method, uri, accessKey, dataType, signatureVersion, timestamp) {	
+  return [method, uri, accessKey, dataType, signatureVersion, timestamp].join('\n');
+}
+
+function sign(signString, accessSecret) {
+  console.log('Llamada a sign');
+  return crypto.createHmac('sha1', accessSecret)
+    .update(new Buffer.from(signString, 'utf-8'))
+    .digest().toString('base64');
+}
+
 app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '10mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
 
 app.use((request, response, next) => {
   var current_data = new Date();
-  var timestamp = current_data.getTime()/1000;
+  var timestamp = current_data.getTime()/1000;  
   console.log('Hora: ' + current_data);
-  console.log(request.headers);
+  console.log('Headers: ');
+  console.log(request.headers);  
   next();
 });
 
@@ -39,24 +62,40 @@ app.use((request, response, next) => {
   next();
 })
 
-app.post('/identify', (request, response) => {
-	console.log('Request dataFile-> ' + request.params['param']);
-	console.log('response-> ' + response);
+app.post('/identify', bodyParser.json(),(request, response) => {
+	const body = request.body;	
 	
+	//console.log('Request RESPONSE-> ');
+	//console.log('Request RESPONSE-> ');
+	//response.json(body);
+	var signature = sign(stringToSign, options.access_secret);	
+	var bitmap = fs.readFileSync(body.param);
+	var formData = {
+    sample: new Buffer(bitmap),
+    access_key:options.access_key,
+    data_type:options.data_type,
+    signature_version:options.signature_version,
+    signature:signature,
+    //sample_bytes:body.length,	
+	sample_bytes:1000000,
+    timestamp:timestamp
+	}	
+	//console.log(formData);	
 	requestI.post({
     url: "http://"+options.host + options.endpoint,
     method: 'POST',
-    formData: ''
+    formData:  formData
 	}, function (err, httpResponse, body) {
-	  if (err) console.log(err);
-	  console.log(body);
-	  //return httpResponse;
+	  if (err) console.log(err);		  
+	  console.log('Estamos en requestI.post');	  
+	  console.log('statusCode:', httpResponse && httpResponse.statusCode); 
+	  console.log('SERVER RESPONSE BODY' + body);
+	  response.json(body);
+	  return httpResponse;
 	});	
+
 	
-  response.json({
-    chance: request.chance,
-	options: options
-  })
+ 
 });
 
 app.listen(3000);
